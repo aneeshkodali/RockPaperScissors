7195c554a50554bdbd30c0f364d9b35a204dc4f9
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-05-20 14:40:02 -0400

    Write Functions for Playing Game

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // 'Caching the DOM'
 // Caching - storing something for future use
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
-const compueterScore_span = document.getElementById("computer-score");
+const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
@@ -20,9 +20,78 @@ function getComputerChoice() {
     return choices[randomNumber];
 }
 
+// function to convert choice strings to words
+function convertToWord(letter) {
+    if (letter==="r") {return "Rock"};
+    if (letter==="p") {return "Paper"};
+    if (letter==="s") {return "Scissors"};
+}
+
+// function for when user wins
+function win(userChoice, computerChoice) {
+    
+    // increase user score
+    userScore++;
+    // update user score element on page
+    userScore_span.innerHTML = userScore;
+    // update computer score element on page
+    computerScore_span.innerHTML = computerScore;
+    // update the result message element on page 
+    const smallUserWord = "user".fontsize(3).sub();
+    const smallComputerWord = "comp".fontsize(3).sub();
+    result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} beats ${convertToWord(computerChoice)}${smallComputerWord}. You win! :)`;
+}
+
+// function for when user loses
+function lose(userChoice, computerChoice) {
+
+    // increase computer score
+    computerScore++;
+    // update user score element on page
+    userScore_span.innerHTML = userScore;
+    // update computer score element on page
+    computerScore_span.innerHTML = computerScore;
+    // update the result message element on page 
+    const smallUserWord = "user".fontsize(3).sub();
+    const smallComputerWord = "comp".fontsize(3).sub();
+    result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} loses to ${convertToWord(computerChoice)}${smallComputerWord}. You lose :(`;
+
+}
+
+// function for draw
+function draw(userChoice, computerChoice) {
+    // update the result message element on page 
+    const smallUserWord = "user".fontsize(3).sub();
+    const smallComputerWord = "comp".fontsize(3).sub();
+    result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} equals ${convertToWord(computerChoice)}${smallComputerWord}. It's a draw`;
+
+}
+
 // Define game function - compare user choice ('input') against computer
 function game(userChoice) {
+
+    // get computer choice
+    const computerChoice = getComputerChoice();
     
+    // Compare user and computer
+    switch (userChoice + computerChoice) {
+        case "rs":
+        case "sp":
+        case "pr":
+            win(userChoice, computerChoice);
+            break;
+        case "sr":
+        case "ps":
+        case "rp":
+            lose(userChoice, computerChoice);
+            break;
+        case "rr":
+        case "pp":
+        case "ss":
+            draw(userChoice, computerChoice);
+            break;
+    }
+
 }
 
 // Add main function (encompases all)
