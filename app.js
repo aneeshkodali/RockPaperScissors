a16e00d9af399eff743c2a2237774745708d4cdb
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-05-20 13:18:55 -0400

    Add Event Listeners and Computer Choice

--- app.js
+++ app.js
@@ -9,3 +9,37 @@ const result_div = document.querySelector(".result");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
+
+// computer choice function - random choice
+function getComputerChoice() {
+    const choices = ['r', 'p', 's'];
+    // generate random number
+    const randomNumber = Math.floor(Math.random()*choices.length);
+
+    // return choice based on element of random number in choices array
+    return choices[randomNumber];
+}
+
+// Define game function - compare user choice ('input') against computer
+function game(userChoice) {
+    
+}
+
+// Add main function (encompases all)
+function main () {
+
+    //Add event listeners
+    rock_div.addEventListener('click', function() {
+        game("r");
+    })
+
+    paper_div.addEventListener('click', function() {
+        game("p");
+    })
+
+    scissors_div.addEventListener('click', function() {
+        game("s");
+    })
+}
+
+main();
