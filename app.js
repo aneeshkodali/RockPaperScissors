bef548f98175ba27fd045438c32254f6116e3231
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-05-27 21:40:47 -0400

    Add Lizard and Spock

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const result_p = document.querySelector('.result > p');
 const rock_div = document.getElementById('r');
 const paper_div = document.getElementById('p');
 const scissors_div = document.getElementById('s');
+const lizard_div = document.getElementById('l');
+const spock_div = document.getElementById('sp');
 
 const choices_div = document.getElementById('choices');
 
